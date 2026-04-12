3ff2dc109bb81b589e19f86a963be167ea09982c
OscarGI <al346148@edu.uaa.mx>
2026-04-11 18:54:51 -0600

    Fix: Se agrego ruta del carrito

--- alexandriaUnburnt/src/app/app.routes.ts
+++ alexandriaUnburnt/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { InicioComponent } from './components/inicio.component/inicio.component'
 import { ProductsComponent } from './components/products.component/products.component';
 import {BookComponent} from './components/book.component/book.component';
 import { ContactoComponent } from './components/contacto.component/contacto.component';
+import { CarritoComponent } from './components/carrito.component/carrito.component';
 
 export const routes: Routes = [{
     path: '',
@@ -16,6 +17,10 @@ export const routes: Routes = [{
     path: 'catalogo/:isbn',
     component: BookComponent,
     runGuardsAndResolvers: 'always'
+  },
+  {
+    path: 'carrito',
+    component: CarritoComponent
   },
   {
     path: 'contacto',
